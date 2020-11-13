23c0172f6fdf62b1dd96be5bdf4b54367493c4ca
Phat244 <71125470+Phat244@users.noreply.github.com>
2020-11-13 17:21:46 +0700

    Update show.php

--- show.php
+++ show.php
@@ -23,6 +23,7 @@ while($Result = mysqli_fetch_array($res))
 {
 ?>
   <tr>
+    <td><?php echo $Result['ID'];?></div></td>
     <td><?php echo $Result['Name'];?></div></td>
     <td><?php echo $Result['Comment'];?></td>
     <td><?php echo $Result['Link'];?></td>
