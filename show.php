a64c9c57cff2862e2b229030a5be1d98ce37af9d
Phat244 <71125470+Phat244@users.noreply.github.com>
2020-11-19 22:32:03 +0700

    Update show.php

--- show.php
+++ show.php
@@ -20,7 +20,7 @@ if (mysqli_connect_errno($conn))
 }
 $res = mysqli_query($conn, 'SELECT * FROM guestbook');
 ?>
-<nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
+<nav class="alert alert-primary" role="alert" id="mainNav">
     <div class="container">
       <a class="navbar-brand" href="show.php">Start Bootstrap</a>
       <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
