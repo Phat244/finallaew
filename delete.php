de0aa66bf49ed9194456232f63104527854ee5d5
Phat244 <71125470+Phat244@users.noreply.github.com>
2020-11-20 15:44:50 +0700

    Update delete.php

--- delete.php
+++ delete.php
@@ -25,12 +25,10 @@ $sql = "DELETE FROM guestbook WHERE ID=$id";
 if ($conn->query($sql) === TRUE) {
   echo ('<div class="container"><h1 class="display-1">Do you really want to delete? </h1></div>');
   echo (' <form method="get" action="show.php">
-          <button type="submit" class="btn btn-danger">Yes</button>
+          <button type="submit" class="btn btn-danger">Continue</button>
           </form>');
-  if ($conn->query($sql) === TRUE) {
-  echo (' <button type="submit" class="btn btn-danger">No</button>
-          </form>');
- 
+
+  
 } else {
   echo "Error deleting record: " . $conn->error;
 }
